94f7c954fdeda539202a356c73ef0e56ae7b40ce
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-23 11:13:56 -0400

    configuring chakra ui in the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Center } from "@chakra-ui/react";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center ">
-      Hello world!
-    </h1>
-  )
+    <>
+      <Center bg="tomato" h="50px" color="white">
+        Hello world from Chakra UI!
+      </Center>
+      <h1 className="text-3xl font-bold underline text-center ">
+        Hello world from Tailwind CSS!
+      </h1>
+    </>
+  );
 }
