c15aaa24453a9988087c4bb2eba0f19a69b1655c
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-24 15:30:48 -0400

    configuring vite as PWA according to the DOC https://vite-plugin-pwa.netlify.app/

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import ReloadPrompt from "./ReloadPrompt";
 
 ReactDOM.render(
   <React.StrictMode>
+    <ReloadPrompt />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Budget-App",
+        short_name: "Budget",
+        theme_color: "#333333",
+        background_color: "#160312",
+        description:
+          "This budget app is a tool to keep reflected in monetary amounts, expenses and current income of a specific person.",
+        lang: "es",
+        icons: [
+          {
+            src: "./assets/favicon.svg",
+            sizes: "212x150",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
+});
