3faa8053d0d0453c7a199ad5956342bb8fe00ff0
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-04-26 18:25:39 -0400

    feat: Add MUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import { ReloadPrompt } from './components';
 import AppHome from './App';
+import theme from './theme';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-const rootElement = document.getElementById('root');
-render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ReloadPrompt />
-    <AppHome />
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <ReloadPrompt />
+      <AppHome />
+    </ThemeProvider>
   </React.StrictMode>,
-  rootElement,
 );
